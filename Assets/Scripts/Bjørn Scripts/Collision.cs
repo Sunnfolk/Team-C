638ceae0b0ad7798df7bf598805c55f4015139c3
Synneogbjo <91120023+Synneogbjo@users.noreply.github.com>
2021-09-27 13:13:50 +0200

    Added grid

--- Assets/Scripts/Bjørn Scripts/Collision.cs
+++ Assets/Scripts/Bjørn Scripts/Collision.cs
@@ -13,6 +13,7 @@ public class Collision : MonoBehaviour
 
     //TODO Add deathpit collision
 
+    //Uses a start position "pos" and a distance "dist" to check if a point under the start position is "ground"
     public bool IsGrounded(Vector3 pos, float dist)
     {
         Debug.DrawRay(pos, Vector2.down, new Color(1f,0f,1f));
