c05c3b4d7f6ed2d8e5c0b001929eaa78792bd425
Synneogbjo <91120023+Synneogbjo@users.noreply.github.com>
2021-09-29 22:17:27 +0200

    test

---
+++ Assets/Scripts/Bjørn Scripts/TimmyAudio.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TimmyAudio : MonoBehaviour
+{
+    public AudioClip jump;
+
+    private AudioSource _AudioSource;
+    private PlayerInput _Input;
+    private PlayerMovement _Movement;
+
+    private void Start()
+    {
+        _AudioSource = GetComponent<AudioSource>();
+        _Input = GetComponent<PlayerInput>();
+        _Movement = GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+    {
+        JumpAudio();
+    }
+
+    private void JumpAudio()
+    {
+        if (_Input.jump && _Movement.canCoyote)
+        {
+            _AudioSource.pitch = Random.Range(0.95f, 1.1f);
+            _AudioSource.volume = 0.4f;
+            _AudioSource.PlayOneShot(jump);
+        }
+    }
+}
