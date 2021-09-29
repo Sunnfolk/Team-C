707a7dd7fc2cedadd0b60cf2e4d93e9bf806c3c8
Synneogbjo <91120023+Synneogbjo@users.noreply.github.com>
2021-09-29 19:54:01 +0200

    test

--- Assets/Scripts/Bjørn Scripts/CameraMoveVertical.cs
+++ Assets/Scripts/Bjørn Scripts/CameraMoveVertical.cs
@@ -1,14 +1,44 @@
-using System;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraMoveVertical : MonoBehaviour
 {
     private PlayerInput _Input;
     [SerializeField] private float maxOffset = 2f;
-    private float offset;
+    private float _Offset;
+    private float _Timer;
+    [SerializeField] private float _TimeBeforeCameraMove = 0.5f;
+    [SerializeField] private float _MoveSpeed = 0.6f;
+    
+    /* Cinemachine */
+    [SerializeField] private CinemachineCore _Transposer;
 
     private void Start()
     {
         _Input = GetComponent<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        if (_Input.moveVector.y > 0)
+        {
+            _Timer -= Time.deltaTime;
+            if (_Timer <= 0)
+            {
+                
+            }
+        }
+        else if (_Input.moveVector.y > 0)
+        {
+            _Timer -= Time.deltaTime;
+            if (_Timer <= 0)
+            {
+
+            }
+        }
+        else
+        {
+            _Timer = _TimeBeforeCameraMove;
+        }
     }
 }
